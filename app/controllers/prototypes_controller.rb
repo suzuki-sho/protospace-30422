fb9520bcf161ecec12fc0b186f11d05b489a2103
suzuki-sho <mrmyself.chun1000ten@gmail.com>
2020-10-03 12:05:11 +0900

    新規投稿＆編集機能修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -50,6 +50,6 @@ class PrototypesController < ApplicationController
 
   private
   def prototype_params
-    params.permit(:title, :catch_copy, :concept,:image).merge(user_id: current_user.id)  
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)  
   end
 end
